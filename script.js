a55fb130d2433030c9cdb39201e79c5c6876522d
Daniel Valdes <valdes14507@gmail.com>
2026-03-16 15:40:20 -0500

    Refactor Firebase setup in script.js
    Updated Firebase configuration and initialization code.

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
-apiKey: "AQUI",
-authDomain: "AQUI",
-databaseURL: "AQUI",
-projectId: "AQUI"
-
+  apiKey: "AIzaSyCAH-sgIsTke35M1WZE5mZqUu4OnxoYETw",
+  authDomain: "registro-tareas-1915d.firebaseapp.com",
+  projectId: "registro-tareas-1915d",
+  storageBucket: "registro-tareas-1915d.firebasestorage.app",
+  messagingSenderId: "125646839483",
+  appId: "1:125646839483:web:7a94914e78bc8aa00a139e",
+  measurementId: "G-ES1PMTGN9T"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 
 firebase.initializeApp(firebaseConfig);
 
