df174d2b30f9d241352f9b9fbacdf8fe42f49584
Daniel Valdes <valdes14507@gmail.com>
2026-03-16 16:13:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,4 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyCAH-sgIsTke35M1WZE5mZqUu4OnxoYETw",
-    authDomain: "registro-tareas-1915d.firebaseapp.com",
-    databaseURL: "https://registro-tareas-1915d-default-rtdb.firebaseio.com",
-    projectId: "registro-tareas-1915d",
-    storageBucket: "registro-tareas-1915d.firebasestorage.app",
-    messagingSenderId: "125646839483",
-    appId: "1:125646839483:web:7a94914e78bc8aa00a139e",
-    measurementId: "G-ES1PMTGN9T"
-  };
-
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
 
 firebase.initializeApp(firebaseConfig);
 
