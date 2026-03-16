d5e10a45329e3edcb5539905c7889dd77cee731a
Daniel Valdes <valdes14507@gmail.com>
2026-03-16 15:50:33 -0500

    Update Firebase imports to use CDN links

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-analytics.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyCAH-sgIsTke35M1WZE5mZqUu4OnxoYETw",
-  authDomain: "registro-tareas-1915d.firebaseapp.com",
-  projectId: "registro-tareas-1915d",
-  storageBucket: "registro-tareas-1915d.firebasestorage.app",
-  messagingSenderId: "125646839483",
-  appId: "1:125646839483:web:7a94914e78bc8aa00a139e",
-  measurementId: "G-ES1PMTGN9T"
-};
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyCAH-sgIsTke35M1WZE5mZqUu4OnxoYETw",
+    authDomain: "registro-tareas-1915d.firebaseapp.com",
+    projectId: "registro-tareas-1915d",
+    storageBucket: "registro-tareas-1915d.firebasestorage.app",
+    messagingSenderId: "125646839483",
+    appId: "1:125646839483:web:7a94914e78bc8aa00a139e",
+    measurementId: "G-ES1PMTGN9T"
+  };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const analytics = getAnalytics(app);
+</script>
 
 firebase.initializeApp(firebaseConfig);
 
